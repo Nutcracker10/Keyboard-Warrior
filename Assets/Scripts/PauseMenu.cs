26cc563765cd53fd7779d3c4b0744e235443cad9
James <jamesgkirwan@gmail.com>
2021-04-01 18:08:20 +0100

    Menu button in pause menu now brings you to main menu scene

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void Resume() {
+    public void Resume() {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         gameIsPaused = false;
@@ -32,7 +33,7 @@ public class PauseMenu : MonoBehaviour
     }
 
     public void LoadMenu() {
-
+        SceneManager.LoadScene("MenuScene");
     }
 
     public void QuitGame() {
